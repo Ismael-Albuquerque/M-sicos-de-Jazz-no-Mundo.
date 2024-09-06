a27e9192995c97e04774bbbb2470ba68fa2e6e75
Ismael Albuquerque <46321386+Ismael-Albuquerque@users.noreply.github.com>
2024-09-06 18:15:37 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    titulo : "Louis Armstrong",
+    descricao : "Louis Armstrong, o icônico trompetista e vocalista, é considerado um dos músicos mais influentes de todos os tempos. Sua voz rouca e única, combinada com sua virtuosidade instrumental, revolucionou o jazz e deixou um legado duradouro na música popular. Nascido em Nova Orleans, berço do jazz, Armstrong se tornou um símbolo da cultura afro-americana e um embaixador da música americana no mundo. Suas canções, como What a Wonderful World e Hello, Dolly!, continuam a encantar e inspirar gerações.",
+    link : "https://pt.wikipedia.org/wiki/Louis_Armstrong#:~:text=Ele%20%C3%A9%20considerado%20uma%20das,not%C3%B3rio%20por%20suas%20habilidades%20como",
+    tags: "trompete jazz trompetista vocalista"
+  },
+  {
+    titulo : "Miles Davis",
+    descricao : "Miles Davis, um dos músicos de jazz mais inovadores e influentes do século XX, revolucionou o gênero com seu som cool e experimental. Conhecido por sua versatilidade e por ter liderado diversos movimentos musicais, Davis deixou um legado que continua a inspirar músicos até hoje.",
+    link : "https://pt.wikipedia.org/wiki/Miles_Davis",
+    tags: "jazz trompete trompetista inovador musico"
+  },
+  {
+    titulo : "Arturo Sandoval",
+    descricao : "Arturo Sandoval é um trompetista cubano, pianista, compositor e líder de banda, conhecido por sua virtuosidade técnica e seu som brilhante. Ele é um dos mais importantes músicos de jazz latino e tem sido uma figura influente na cena musical internacional.",
+    link : "https://pt.wikipedia.org/wiki/Arturo_Sandoval",
+    tags: "jazz trompetista pianista compositor"
+  }
+];
+
+//CRIANDO UM OBJETO
+
